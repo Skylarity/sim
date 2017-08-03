29275b4c5200828a2d656fdf47088935afe257b6
Skyler Rexroad <skyrex1095@gmail.com>
2017-08-02 19:37:54 -0600

    Added initial component

--- src/main.js
+++ src/main.js
@@ -4,12 +4,18 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import 'normalize.css'
+import 'vue-awesome/icons'
+import Icon from 'vue-awesome/components/Icon'
+
+Vue.component('icon', Icon)
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  el: '#app',
-  router,
-  template: '<App/>',
-  components: { App }
+	el: '#app',
+	router,
+	template: '<App/>',
+	components: { App }
 })
